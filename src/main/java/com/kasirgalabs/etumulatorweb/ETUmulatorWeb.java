86af0f0fbcceb0c872a0bd11bc0b446322f4278f
Görkem Mülayim <gorkemmulayim@icloud.com>
2018-03-04 14:27:34 +0300

    Initial project structure.

---
+++ src/main/java/com/kasirgalabs/etumulatorweb/ETUmulatorWeb.java
@@ -0,0 +1,25 @@
+package com.kasirgalabs.etumulatorweb;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import java.io.IOException;
+import java.net.URI;
+
+public class ETUmulatorWeb {
+    private static final URI BASE_URI = URI.create("http://localhost:8080/");
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        ResourceConfig resourceConfig = new ResourceConfig();
+        HttpServer httpServer = GrizzlyHttpServerFactory.createHttpServer(BASE_URI, resourceConfig, false);
+        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+            @Override
+            public void run() {
+                httpServer.shutdownNow();
+            }
+        }));
+        httpServer.start();
+        Thread.currentThread().join();
+    }
+}
