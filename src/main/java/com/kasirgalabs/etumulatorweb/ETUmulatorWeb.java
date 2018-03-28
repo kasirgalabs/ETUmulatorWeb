cebd4b67c20508b191a86a70dde83a332a92f6e0
Görkem Mülayim <gorkemmulayim@icloud.com>
2018-03-28 15:34:22 +0300

    Fixed homepage path proplem. #2

--- src/main/java/com/kasirgalabs/etumulatorweb/ETUmulatorWeb.java
+++ src/main/java/com/kasirgalabs/etumulatorweb/ETUmulatorWeb.java
@@ -1,5 +1,6 @@
 package com.kasirgalabs.etumulatorweb;
 
+import com.kasirgalabs.etumulatorweb.pages.Homepage;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
@@ -11,14 +12,9 @@ public class ETUmulatorWeb {
     private static final URI BASE_URI = URI.create("http://localhost:8080/");
 
     public static void main(String[] args) throws IOException, InterruptedException {
-        ResourceConfig resourceConfig = new ResourceConfig().packages("com.kasirgalabs.etumulatorweb");
+        ResourceConfig resourceConfig = new ResourceConfig().register(Homepage.class);
         final HttpServer httpServer = GrizzlyHttpServerFactory.createHttpServer(BASE_URI, resourceConfig, false);
-        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
-            @Override
-            public void run() {
-                httpServer.shutdownNow();
-            }
-        }));
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> httpServer.shutdownNow()));
         httpServer.start();
         Thread.currentThread().join();
     }
